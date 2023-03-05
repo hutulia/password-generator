6e20ea8c7378604a372778c4e0ef55c6a7b6f245
vitaliy bondarenko <bovile17051992@gmail.com>
2023-03-05 21:07:29 +0200

    Refact renew btn

--- src/ui/renew-button.js
+++ src/ui/renew-button.js
@@ -1,30 +1,31 @@
 import {PasswordBuilder} from "../password-builder.js";
 import {PasswordAsText} from "./password-as-text.js";
 
-export class RenewButton{
+export class RenewButton {
     /**
-     *
      * @type {Element}
      */
     button = null;
 
     /**
-     *
      * @type {PasswordBuilder}
      */
     passwordBuilder = null;
 
     /**
-     *
      * @type {PasswordAsText}
      */
     passwordAsText = null;
 
+    /**
+     * @param {Element} button
+     * @param {PasswordBuilder} passwordBuilder
+     * @param {PasswordAsText} passwordAsText
+     */
     constructor(button, passwordBuilder, passwordAsText) {
         this.button = button;
         this.passwordBuilder = passwordBuilder;
         this.passwordAsText = passwordAsText;
-
-        this.button.addEventListener('click',()=>{this.passwordAsText.setPassword(this.passwordBuilder.build())});
+        this.button.onclick = () => this.passwordAsText.setPassword(this.passwordBuilder.build());
     }
 }

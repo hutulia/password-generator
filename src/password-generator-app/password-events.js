befa186da007421df3574982898912ec0908fcdb
vitaliy bondarenko <bovile17051992@gmail.com>
2023-03-07 20:44:49 +0200

    Refact

--- src/password-generator-app/password-events.js
+++ src/password-generator-app/password-events.js
@@ -1,4 +1,6 @@
 export class PasswordEvents{
     static UPDATED = 'UPDATED';
     static COPIED = 'COPIED';
+    static USE_SYMBOLS_SET = 'USE_SYMBOLS_SET';
+    static DO_NOT_USE_SYMBOLS_SET = 'DO_NOT_USE_SYMBOLS_SET';
 }

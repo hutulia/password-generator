4ff76831e22c15f51aba096277907fd12b11a61d
vitaliy bondarenko <bovile17051992@gmail.com>
2023-03-10 00:29:59 +0200

    Change to react

--- ui/react/controls/predefined-length.js
+++ ui/react/controls/predefined-length.js
@@ -5,15 +5,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {PasswordEvents} from "../../../src/password-generator-app/password-events";
 export function PredefinedLength({passwordBuilder, length}) {
+    const [active, setActive] = React.useState(length == passwordBuilder.getLength());
     const click = (e) => {
         passwordBuilder.setLength(e.target.innerHTML).build();
     };
 
-    // React.useEffect(() => {
-    //     password.getElement().addEventListener(PasswordEvents.UPDATED,()=>setLength(password.getLength()));
-    // },[]);
+    React.useEffect(() => {
+        passwordBuilder.getEvents().on(PasswordEvents.LENGTH_UPDATED,()=>setActive(length == passwordBuilder.getLength()));
+    },[]);
+
+    let classList = ['btn', 'predefined-length'];
+    if(active){
+        classList.push('active');
+    }
 
     return (
-        <button className="predefined-length pl{length}" onClick={click}>{length}</button>
+        <button className={classList.join(' ')} onClick={click}>{length}</button>
     );
 }
